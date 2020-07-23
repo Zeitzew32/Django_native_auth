653bb7bb13d593b16edfc5e6759866d07be2d645
Jacob <zeitzew32@gmail.com>
2020-07-22 23:07:51 -0700

    Import refactoring

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
-from django.db import IntegrityError
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.utils import timezone
 from .forms import TodoForm
 from .models import Todo
-from django.utils import timezone
-from django.contrib.auth.decorators import login_required
+
+
 
 
 def home(request):
